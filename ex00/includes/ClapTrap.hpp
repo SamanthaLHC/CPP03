5e13ac87f0b6f3138d952ce6eb367e9e86700bde
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-25 18:36:11 +0100

    add defin ClaTrap

--- ex00/includes/ClapTrap.hpp
+++ ex00/includes/ClapTrap.hpp
@@ -6,14 +6,37 @@
 /*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:09:06 by sam               #+#    #+#             */
-/*   Updated: 2022/11/25 16:10:50 by sam              ###   ########.fr       */
+/*   Updated: 2022/11/25 18:27:27 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
 #include <iostream>
 #include <string>
+
+class ClapTrap
+{
+	public:
+
+		ClapTrap::ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const &cpy);
+		ClapTrap &operator=( ClapTrap const &rhs);
+		~ClapTrap(void);
+
+
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+	private:
+
+		int _hit_points;
+		int _energy_points;
+		int _attack_dammage;
+		std::string _name;
+};
 
 #endif
--- ex00/srcs/ClapTrap.cpp
+++ ex00/srcs/ClapTrap.cpp
@@ -6,8 +6,77 @@
 /*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:08:52 by sam               #+#    #+#             */
-/*   Updated: 2022/11/25 16:09:22 by sam              ###   ########.fr       */
+/*   Updated: 2022/11/25 18:34:36 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include "colors.h"
+
+//constructs ans destruct======================================================
+//=============================================================================
+
+ClapTrap::ClapTrap(void) : _name(""), _hit_points(10), _energy_points(10), _attack_dammage(0)
+{
+	std::cout << BWHT << "Default constructor called." << RES << std::endl;
+	return;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hit_points(10), _energy_points(10), _attack_dammage(0)
+{
+	std::cout << BWHT << " Name in param constructor called." << RES << std::endl;
+	return;
+}
+
+ClapTrap::ClapTrap(ClapTrap const& cpy)
+{
+	std::cout << BWHT << "Copy constructor called." << RES << std::endl;
+	*this = cpy;
+}
+
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << BWHT << "Destructor called" << RES << std::endl;
+	return;
+}
+
+// operator overload===========================================================
+//=============================================================================
+
+ClapTrap& ClapTrap::operator=( ClapTrap const& rhs)
+{
+	this->_attack_dammage = rhs._attack_dammage;
+	this->_energy_points = rhs._energy_points;
+	this->_hit_points = rhs._hit_points;
+	this->_name = rhs._name;
+	return (*this);
+}
+
+// members functions===========================================================
+//=============================================================================
+
+//		get / set ?? (on a besoin d'un getter que si on a besoin d'y acceder
+// depuis l'extérieur de la classe (dans le main pou un autre objet qui n'est pas
+// de  la mm classe) )
+
+// int _hit_points
+
+// int _energy_points
+
+// int _attack_dammage
+
+void ClapTrap::attack(const std::string &target)
+{
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	
+}
