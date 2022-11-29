39b28d73dc428a5d2995429051fd319cbf4e4112
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-29 11:46:27 +0100

    delete scenario 3

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:08:57 by sam               #+#    #+#             */
-/*   Updated: 2022/11/29 11:38:28 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/11/29 11:46:03 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,10 @@ int main()
 		}
 		character_one.attack(chara_target.get_name());
 	}
+
+// Vérifier hits points=========================================================
+// =============================================================================
+	
 	{
 		std::cout << BCYN << "SCENARIO 2: Clapy hits target with a club until target has no more hit points."
 				  << RES << std::endl;
@@ -45,26 +49,5 @@ int main()
 			std::cout << chara_target.get_hit_points() << std::endl;
 		}
 		chara_target.beRepaired(repair_points);
-	}
-	{
-		std::cout << BCYN << "SCENARIO 3: Clapy get hits from target with a club and tries to repair himself until he has no more hit points."
-				  << RES << std::endl;
-		
-		ClapTrap character_one("Clapy");
-		ClapTrap chara_target("target");
-		const int club_damage = 5;
-		const int repair_points = 2;
-
-		chara_target.set_attack_damage(club_damage);
-		while (character_one.get_hit_points() > 0)
-		{
-			chara_target.attack(character_one.get_name());
-			character_one.takeDamage(chara_target.get_attack_damage());
-			character_one.beRepaired(repair_points);
-		}
-		// chara_target.attack(character_one.get_name());
-		// character_one.takeDamage(chara_target.get_attack_damage());
-		std::cout << character_one.get_hit_points() << std::endl;
-		character_one.beRepaired(repair_points);
 	}
 }
